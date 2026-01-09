92257514f974ac917e638e1f21b7d063f807a273
Prasanth <prasanthshot@gmail.com>
2026-01-09 15:30:54 +0530

    trying to learn how functional programming works

--- functionalProgramming/intro.js
+++ functionalProgramming/intro.js
@@ -3,5 +3,57 @@ function add(a,b){
     return sum;
 }
 
-let arr = [11,add,12];
-console.log(arr[1](1,2));
+function sub(a,b){
+    let difference = a - b;
+    return difference;
+}
+
+const log = (message) => {
+    console.log(message);
+}
+
+function checkingHowFunctionWorksInArr(){
+    let arr = [11,add,12];
+    console.log(arr[1](1,2));
+}
+
+
+function functionsAsArguments(){
+    function calculator(operation,a,b){
+        let result = operation(a,b);
+        console.log(result);
+    }
+
+    calculator(add,1,2);
+    calculator(sub,5,4);
+}
+
+function functionAsReturnValues(){
+    const createScream = (logger) => {
+        return function(message){
+            logger(message.toUpperCase() + "!!!");
+        }
+    }
+
+    const scream = createScream(message => console.log(message));
+    scream("hello");
+    scream("Dei una thanda");
+}
+
+function usingMultipleArrowFunctions(){
+    function convertToUpperCase(message){
+        return message.toUpperCase();
+    }
+
+    const convertCase = specificConvertion => {
+        return (message) => log(specificConvertion(message))
+    }
+
+    let upperCaseFunction = convertCase(convertToUpperCase);
+    upperCaseFunction("Hello moni.");
+}
+
+// checkingHowFunctionWorksInArr();
+// functionsAsArguments();
+// functionAsReturnValues();
+usingMultipleArrowFunctions();
